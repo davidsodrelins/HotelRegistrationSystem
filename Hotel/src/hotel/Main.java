1b29b1eaad3515c43815454eb972a2f973c60d1d
Carla Regina - DEV <carla_regina_1313@hotmail.com>
2021-04-03 20:56:03 -0300

    hotel customer registration

---
+++ Hotel/src/hotel/Main.java
@@ -0,0 +1,12 @@
+
+package hotel;
+/*
+ * @author CarlaRegina-Dev
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ Hotel/src/hotel/br/com/hotel/Model/TipoQuarto.java
@@ -0,0 +1,20 @@
+
+package hotel.br.com.hotel.Model;
+/*
+ * @author CarlaRegina-Dev
+ */
+public class TipoQuarto {
+     private String descricao;
+     
+     public TipoQuarto(String descricao) {
+        this.descricao = descricao;
+    }
+
+        public String getDescricao() {
+            return descricao;
+        }
+
+        public void setDescricao(String descricao) {
+            this.descricao = descricao;
+        }    
+}
